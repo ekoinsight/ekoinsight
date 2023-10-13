286601b3ad1c7f162a60668539efac5ee013d3dc
Omar Waldmann <omar.waldmann@ca-gip.fr>
2023-10-13 02:21:44 -0400

    use specified id

--- tamagoshi-api/models/user_model.go
+++ tamagoshi-api/models/user_model.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	Id     primitive.ObjectID `json:"id,omitempty"`
+	Id     primitive.ObjectID `json:"id,omitempty" validate:"required"`
 	Name   string             `json:"name,omitempty" validate:"required"`
 	Mail   string             `json:"mail,omitempty" validate:"required"`
 	Health int                `json:"health,omitempty"`
