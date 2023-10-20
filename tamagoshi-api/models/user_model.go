3106da167d844c3c6f9fb2070ca3fc8093d04654
Omar Waldmann <omar.waldmann@ca-gip.fr>
2023-10-19 23:52:56 -0400

    show health when equal 0

--- tamagoshi-api/models/user_model.go
+++ tamagoshi-api/models/user_model.go
@@ -4,5 +4,5 @@ type User struct {
 	Id     string 						`json:"id,omitempty" validate:"required"`
 	Name   string             `json:"name,omitempty" validate:"required"`
 	Mail   string             `json:"mail,omitempty" validate:"required"`
-	Health int             	  `json:"health,omitempty" validate:"required"`
+	Health int             	  `json:"health"`
 }
